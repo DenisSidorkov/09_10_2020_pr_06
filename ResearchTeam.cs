ecfc1a4731bcf54e37f8302ba56743fba96b537b
student <student@C251U12>
2020-10-09 15:36:14 +0300

    TimeFrame

--- ResearchTeam.cs
+++ ResearchTeam.cs
@@ -6,6 +6,12 @@ namespace _09_10_2020_pr_06
 {
   class ResearchTeam
   {
+    private string name_of_research;
+    private string organisation;
+    private int reg_num;
+    private enum TimeFrame { };
 
+    public Paper[]  
+  
   }
 }
