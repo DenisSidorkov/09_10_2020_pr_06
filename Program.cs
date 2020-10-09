d5b23b1b58b202f2e9da1a93844a28dcb8ebd225
student <student@C251U12>
2020-10-09 15:15:47 +0300

    Класс Paper

--- Program.cs
+++ Program.cs
@@ -40,6 +40,28 @@ namespace _09_10_2020_pr_06
     }
   }
   
+  public class Paper
+  { 
+    public string title { get; set; } 
+    public Person author { get; set; }
+    public DateTime date_of_pub { get; set; }
+
+    public Paper(string title, Person author, System.DateTime date_of_pub)
+    {
+      this.title = title;
+      this.author = author;
+      this.date_of_pub = date_of_pub;
+    }
+
+    public Paper() { title = "123"; author = new Person(); date_of_pub = new DateTime(1, 1, 1970); }
+
+    public override string ToString()
+    {
+      return title + " " + author + ", " + date_of_pub;
+    }
+
+  }
+
   class Program
   {
     static void Main(string[] args)
