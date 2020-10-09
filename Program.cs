a6ea3a677d0244f010d94c35a7fc284f295585bb
student <student@C251U12>
2020-10-09 14:41:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09_10_2020_pr_06
+{
+
+  public class Person
+  {
+    private string first_name;
+    private string second_name;
+    private System.DateTime DateOfBirth;
+
+    public Person(string first_name, string second_name, System.DateTime DateOfBirth)
+    {
+      this.first_name = first_name;
+      this.second_name = second_name;
+      this.DateOfBirth = DateOfBirth;
+    }
+
+    public Person() { first_name = "John"; second_name = "Smith"; DateOfBirth = new DateTime(1970, 1, 1);}
+    public string First_name { get; set; }
+    public string Second_name { get; set; }
+    public string _DateOfBirth { get; set; }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
